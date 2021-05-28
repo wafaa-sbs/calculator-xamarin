60994ea138697e2cf8b865a654c644680d62c1d8
wafaa-sbs <wafaasbais@gmail.com>
2021-05-28 09:34:33 +0100

    calculator project

---
+++ CalculatorAndroidTut/obj/Debug/110/android/src/mono/MonoPackageManager_Resources.java
@@ -0,0 +1,13 @@
+package mono;
+public class MonoPackageManager_Resources {
+	public static String[] Assemblies = new String[]{
+		/* We need to ensure that "CalculatorAndroidTut.dll" comes first in this list. */
+		"CalculatorAndroidTut.dll",
+		"Xamarin.Android.Support.Annotations.dll",
+		"Xamarin.Android.Support.Compat.dll",
+		"Xamarin.Android.Support.Core.UI.dll",
+		"Xamarin.Android.Support.v7.GridLayout.dll",
+	};
+	public static String[] Dependencies = new String[]{
+	};
+}
